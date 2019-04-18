8ab5ea56e4d16aaa7bdc2aca17f9757b4efc55b5
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-04-18 09:29:37 -0700

    remove stylings to creat blank template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import TicketList from './TicketList';
-import Header from './Header';
-// import { Switch, Route } from 'react-router-dom';
-// import Error404 from './Error404';
 
 function App(){
-  return (
-    <div>
-      <Header/>
-      <TicketList/>
-      <h1 style={{height: '50px'}}></h1>
-    </div>
-  );
+  return();
 }
 
 export default App;
