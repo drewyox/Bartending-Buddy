9c61938a47383fbfff261d3698cb11fee57c06f7
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-05-10 15:10:42 -0700

    add basic outline for a drinks component

---
+++ src/components/Drink.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Drinks() {
+  return(
+    <div>
+      <h1>{name}</h1>
+      <ul>
+        <li>{ingredient}</li>
+        <li>{ingredient}</li>
+        <li>{ingredient}</li>
+        <li>{ingredient}</li>
+        <li>{ingredient}</li>
+        <li>{ingredient}</li>
+        <li>{ingredient}</li>
+      </ul>
+    </div>
+  );
+}
