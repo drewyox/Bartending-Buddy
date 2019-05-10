d57b4f8ea31dfba55a149c96434b1b0ab48e196a
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-05-10 15:30:00 -0700

    forgot to save before last commit

--- src/components/Drink.jsx
+++ src/components/Drink.jsx
@@ -5,6 +5,7 @@ function Drinks() {
   return(
     <div>
       <h1>{name}</h1>
+      //should be able to display all drink types within 
       <ul>
         <li>{ingredient}</li>
         <li>{ingredient}</li>
