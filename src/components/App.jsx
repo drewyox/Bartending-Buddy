a0ddc406c1c137ccfc2c6489c9ffd3d7e1086d77
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-05-20 13:46:58 -0700

    reformat component structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import Header from './Header';
 import SplashPage from './SplashPage';
 import SearchForm from './SearchForm';
+import Gin from './Gin';
+import Rum from './Rum';
+import Vodka from './Vodka';
 import {Switch, Route } from 'react-router-dom';
 
 function App(){
   return(
     <div>
       <Header/>
+      <SearchForm />
       <Switch>
         <Route exact path='/' component={SplashPage} />
-        <Route path='/search' component={SearchForm} />
+        <Route path='/gin' component={Gin} />
+        <Route path='/rum' component={Rum} />
+        <Route path='/vodka' component={Vodka} />
+        <Route path='/whiskey' component={Whiskey} />
       </Switch>
     </div>
   );
