c1457e5338f10f0869e67485386e9d3c5bb4e438
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-05-20 19:35:11 -0700

    add links section to splash page

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -9,11 +9,38 @@ function SplashPage(){
     backgroundImage: `url(${barBackground})`,
     display: 'flex',
     alignItems: 'center',
-  }
+  };
+  var columnStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+  var columnStyleR = {
+
+  };
+  var columnStyleL = {
+
+  };
   return(
-    <div style={backgroundBar}>
-      <div className='introText'>
-        <p>Welcome to The Bar, a place where you can search, upload and store your very own creative concoctions.</p>
+    <div>
+      <div style={backgroundBar}>
+        <div className='introText'>
+          <p>Welcome to The Bar, a place where you can search, upload and store your very own creative concoctions.</p>
+        </div>
+      </div>
+      <div style={columnStyle}>
+        <div>
+        Link to Drink Section Gin
+        </div>
+        <div>
+        Link to Drink Section whiskey
+        </div>
+        <div>
+        Link to Drink Section Rum
+        </div>
+        <div>
+        Link to Drink Section Vodka
+        </div>
       </div>
     </div>
   );
